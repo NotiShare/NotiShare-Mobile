39fe33fbe5fcb2d124e60676bfa496ab2f9d83c0
FezZHD <zhenya535@gmail.com>
2017-05-07 21:44:56 +0300

    init project, update gitignore

---
+++ NotiShare-Mobile/NotiShare/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace NotiShare
+{
+    [Activity(Label = "NotiShare", Theme = "@style/Theme.AppCompat.Light.DarkActionBar",ScreenOrientation = ScreenOrientation.Portrait, MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
